38bfc479c00a7a6d5a285fdde84e75f37f7cffb6
Khaint002 <khainhokkazuo@gmail.com>
2025-06-11 15:36:26 +0700

    update

--- src/pages/Control/control.js
+++ src/pages/Control/control.js
@@ -47,7 +47,7 @@ getWebSocket = async function (value) {
     try {
         const dataItemCabinet = JSON.parse(localStorage.getItem('itemCondition'));
         const qrCodeParts = dataItemCabinet[0].QR_CODE.split(',');
-        const cabinetID = qrCodeParts[3];
+        cabinetID = qrCodeParts[3];
         const relayCount = parseInt(qrCodeParts[2]); // "3K" or "6K"
 
         const dataDevice = await HOMEOSAPP.getDM(
@@ -96,11 +96,20 @@ getWebSocket = async function (value) {
 function handleWSMessage(data, cabinetID, relayCount, qrCodeParts) {
     const txt = data.data;
     const checkValue = txt.split(":");
-
+    
+    
     if (checkValue[0] === cabinetID) {
         clearTimeout(timeoutHandle);
         timeoutHandle = setTimeout(noDataReceived, 120000);
-
+        if (txt.includes("Command: REFRESH") && txt.includes("process: Done")) {
+            updateIndicator('#online', '#ceac30');
+            $('#btn-online').prop('disabled', false)
+            document.getElementById("btn-online").innerHTML = `
+            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" class="bi bi-plugin" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M1 8a7 7 0 1 1 2.898 5.673c-.167-.121-.216-.406-.002-.62l1.8-1.8a3.5 3.5 0 0 0 4.572-.328l1.414-1.415a.5.5 0 0 0 0-.707l-.707-.707 1.559-1.563a.5.5 0 1 0-.708-.706l-1.559 1.562-1.414-1.414 1.56-1.562a.5.5 0 1 0-.707-.706l-1.56 1.56-.707-.706a.5.5 0 0 0-.707 0L5.318 5.975a3.5 3.5 0 0 0-.328 4.571l-1.8 1.8c-.58.58-.62 1.6.121 2.137A8 8 0 1 0 0 8a.5.5 0 0 0 1 0" />
+            </svg>
+        `;
+        }
         // Update power indicator
         updateIndicator('#power', '#d41c1f');
 
