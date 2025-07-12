462ccbbfb858339337c49a43f67a7b5bfaf8617a
Khaint002 <khainhokkazuo@gmail.com>
2025-07-12 09:45:26 +0700

    update

--- src/pages/Control/control.js
+++ src/pages/Control/control.js
@@ -496,26 +496,26 @@ $("#export-condition")
     });
 
 
-$("#settingAlert").on("click", function () {
+$("#change-view").on("click", function () {
     isActive = !isActive;
     if (isActive) {
         $("#view-EMS").addClass("d-none");
         $("#view-OEE").removeClass("d-none");
-        $("#alertIcon")
+        $("#view-icon")
             .attr("class", "bi bi-grid")
             .html(`
                 <path d="M1 2.5A1.5 1.5 0 0 1 2.5 1h3A1.5 1.5 0 0 1 7 2.5v3A1.5 1.5 0 0 1 5.5 7h-3A1.5 1.5 0 0 1 1 5.5zM2.5 2a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5zm6.5.5A1.5 1.5 0 0 1 10.5 1h3A1.5 1.5 0 0 1 15 2.5v3A1.5 1.5 0 0 1 13.5 7h-3A1.5 1.5 0 0 1 9 5.5zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5zM1 10.5A1.5 1.5 0 0 1 2.5 9h3A1.5 1.5 0 0 1 7 10.5v3A1.5 1.5 0 0 1 5.5 15h-3A1.5 1.5 0 0 1 1 13.5zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5zm6.5.5A1.5 1.5 0 0 1 10.5 9h3a1.5 1.5 0 0 1 1.5 1.5v3a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 9 13.5zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5z"/>
             `);
-        $("#alertText").text("Đã bật");
+        $("#view-text").text("Đã bật");
     } else {
         $("#view-EMS").removeClass("d-none");
         $("#view-OEE").addClass("d-none");
-        $("#alertIcon")
+        $("#view-icon")
             .attr("class", "bi bi-list-ul")
             .html(`
                 <path fill-rule="evenodd" d="M5 11.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5m-3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2m0 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2m0 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2"/>
             `);
-        $("#alertText").text("OEE");
+        $("#view-text").text("OEE");
     }
 
     // Gọi hàm xử lý logic nếu cần
