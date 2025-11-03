e8a2964944d76312cc87797614550439d6a76835
Khaint002 <khainhokkazuo@gmail.com>
2025-11-03 11:43:16 +0700

    update

--- src/pages/WareHouse/wareHouse.js
+++ src/pages/WareHouse/wareHouse.js
@@ -1814,12 +1814,13 @@ var handleSaveIdentities = async () => {
                         //     "DM_QRCODE",
                         //     "QR_CODE='" + item + "'"
                         // );
+                        
                         const dataItemQRcode = dataItemQRcodes.data.filter((batch) => batch.QR_CODE === item)
                         
                         console.log(dataLot.data[0].PR_KEY);
-                        console.log(dataItemQRcode.data[0].LOT_ID, dataLot.data[0].PR_KEY);
+                        console.log(dataItemQRcode[0].LOT_ID, dataLot.data[0].PR_KEY);
                         
-                        if(dataItemQRcode.data[0].LOT_ID != dataLot.data[0].PR_KEY){
+                        if(dataItemQRcode[0].LOT_ID != dataLot.data[0].PR_KEY){
                             console.log(dataItemQRcode.data[0]);
                             const original = dataItemQRcode.data[0];
                             const willInsertData = {
